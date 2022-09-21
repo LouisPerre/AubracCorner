3378b563faa0f8eb3c5b1bd2d64c98892a1c1044
Louis Perrenot <louis.perrenot@edu.devinci.net>
2022-09-21 12:17:51 +0200

    ADD - NAVBAR style and effect

--- script.js
+++ script.js
@@ -1,7 +1,46 @@
 const DATE = new Date().getHours()
-const BOX = document.querySelector('.changeColor')
+// const BOX = document.querySelector('.changeColor')
+var htmlElement = document.documentElement;
+var bodyElement = document.body;
 const CHANGETIME = 11
+const documentHeight = Math.max(
+    htmlElement.clientHeight, htmlElement.scrollHeight, htmlElement.offsetHeight,
+    bodyElement.scrollHeight, bodyElement.offsetHeight
+)
 
-if (DATE >= CHANGETIME) {
-    BOX.classList.toggle("late")
+// if (DATE >= CHANGETIME) {
+//     BOX.classList.toggle("late")
+// }
+
+let previousScroll = 0
+window.onscroll = function() {
+    let currentScroll = window.scrollY
+    // console.log(currentScroll)
+
+    if (currentScroll > 0 && currentScroll < documentHeight - window.innerHeight ) {
+        console.log('lala')
+
+        if (currentScroll > previousScroll) {
+            window.setTimeout(hideNav, 300)
+        } else if (currentScroll == previousScroll) {
+            window.setTimeout(showNav, 300)
+        } else {
+            window.setTimeout(showNav, 300)
+        }
+
+        previousScroll = currentScroll
+    }
+}
+
+function hideNav() {
+    document.querySelector('#navigation-scroll').style.display = 'none'
+    document.querySelector(".main-navigation-scroll").classList.remove("is-visible")
+    document.querySelector(".main-navigation-scroll").classList.add("is-hidden")
+}
+
+function showNav() {
+    document.querySelector(".main-navigation-scroll").classList.remove("is-hidden")
+    document.querySelector(".main-navigation-scroll").classList.add("is-visible")
+    document.querySelector('#navigation-scroll').style.display = 'block'
+    document.querySelector(".main-navigation-scroll").classList.add("shadow")
 }
